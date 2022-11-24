386b255b345ef38d1c66630ea6ce5f041a18a1fa
Prahsant Gehlot <gehlotprashant968@gmail.com>
2022-11-24 17:29:32 +0530

    added apps to settings

--- Bhoomi/Bhoomi/settings.py
+++ Bhoomi/Bhoomi/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'blogs',
+    'home',
+    'complaints',
+    'donations',
+    'newsletter',
 ]
 
 MIDDLEWARE = [
