5bb74b229b7c71b6088084cba3d12aea59cf1f2b
Prashant9683 <amenu4elc21048@am.students.amrita.edu>
2022-11-24 21:47:38 +0530

    fixed some errors

--- Bhoomi/blogs/admin.py
+++ Bhoomi/blogs/admin.py
@@ -3,12 +3,12 @@ from .models import Category, Post
 # Register your models here.
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['img-tg', 'title' , 'description']
+    list_display = ['img_tg', 'title' , 'description']
     search_fields = ('title', )
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title']
     search_fields = ('title', )
-    prepopulated_fields = {'slug': ('title',)}
+    # prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
