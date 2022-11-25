539e999ed0756415344f69f6fd8d743218e323dd
Prashant9683 <amenu4elc21048@am.students.amrita.edu>
2022-11-25 12:19:41 +0530

    added complaint model

--- Bhoomi/complaints/models.py
+++ Bhoomi/complaints/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Complaint(models.Model):
+    name = models.CharField(max_length=100)
+    id = models.AutoField(primary_key=True)
+    reportnumber = models.CharField(max_length=100, null=True, blank=True)
+    raised_dt = models.DateTimeField(auto_now_add=True)
+    complaint_type = models.CharField(max_length=100, null=False, blank=False)
+    location = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.reportnumber
