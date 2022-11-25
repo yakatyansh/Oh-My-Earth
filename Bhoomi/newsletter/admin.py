b69e326c6fde608f35b5230c15040495a46ef3af
Prashant9683 <amenu4elc21048@am.students.amrita.edu>
2022-11-25 16:52:26 +0530

    made complaint and donations

--- Bhoomi/newsletter/admin.py
+++ Bhoomi/newsletter/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Subscriber
+from .models import Newsletter
 
-# Register your models here.
+
+def send_newsletter(modeladmin, request, queryset):
+    for newsletter in queryset:
+        newsletter.send(request)
+
+send_newsletter.short_description = "Send selected Newsletters to all subscribers"
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    actions = [send_newsletter]
+
+admin.site.register(Subscriber)
+admin.site.register(Newsletter, NewsletterAdmin)
