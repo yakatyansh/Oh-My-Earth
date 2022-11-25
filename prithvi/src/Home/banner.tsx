b173ccea36a29cc09ff3e0d2abb11ef648638d5a
Gaurav Sharma <gaurav021201@gmail.com>
2022-11-25 17:44:07 +0530

    added reviews and ui improvements

--- prithvi/src/Home/banner.tsx
+++ prithvi/src/Home/banner.tsx
@@ -1,6 +1,8 @@
+import img from "../Assets/get_Started.png"
+
 const Banner = () => {
     return (
-        <section className="my-14">
+        <section className="mt-52">
             <div className="md:w-11/12 w-full md:px-0 px-3 mx-auto">
                 <div className="bg-quiz-bg h-auto sm:bg-cover bg-center bg-no-repeat bg-[#6D9886] rounded-xl relative">
                     <article className="py-24 md:px-14 px-4 md:w-9/12 md:mx-0 mx-auto md:text-left text-center leading-none">
@@ -11,12 +13,12 @@ const Banner = () => {
                             <span className="absolute inset-0 flex items-center justify-center w-full h-full text-black duration-300 -translate-x-full bg-white group-hover:translate-x-0 ease">
                                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                             </span>
-                            <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Button Text</span>
+                            <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Get Started</span>
                             <span className="relative invisible">Get Started</span>
                         </button>
                     </article>
                     <div className="absolute lg:right-[10%] right-[4%] lg:-top-[10%] -top-[5%] md:block hidden">
-                        {/*<img src={quiz_illustration} alt="illustration" className="w-72" />*/}
+                        <img src={img} alt="illustration" className="w-72" />
                     </div>
                 </div>
             </div>
--- prithvi/src/Home/stats.tsx
+++ prithvi/src/Home/stats.tsx
@@ -1,6 +1,6 @@
 const Stats = () => {
     return (
-        <section className="stats_box py-10 grid place-items-center lg:grid-cols-4 grid-cols-2 gap-4 sm:w-9/12 w-11/12 mx-auto mt-2 px-4">
+        <section className="stats_box py-10 grid place-items-center lg:grid-cols-4 grid-cols-2 gap-4 sm:w-9/12 w-11/12 mx-auto mt-16 px-4">
             <div>
                 <h1 className="md:text-[40px] text-[25px] font-bold">120+</h1>
                 <p>Experts and Researchers</p>
